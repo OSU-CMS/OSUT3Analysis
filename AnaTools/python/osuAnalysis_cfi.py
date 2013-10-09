e1d1cbbb28176edc1e6b562f65505f88a9e81115
Andrew Hart <ahart@cern.ch>
2013-10-09 11:22:48 -0400

    Added new parameters for optional electron scale factors from histograms.

--- AnaTools/python/osuAnalysis_cfi.py
+++ AnaTools/python/osuAnalysis_cfi.py
@@ -58,9 +58,11 @@ process.OSUAnalysis = cms.EDProducer ('OSUAnalysis',
 
     puFile = cms.string (os.environ['CMSSW_BASE']+'/src/OSUT3Analysis/Configuration/data/pu.root'),
     deadEcalFile = cms.string (os.environ['CMSSW_BASE']+'/src/OSUT3Analysis/Configuration/data/DeadEcalChannels.txt'),
+    electronSFFile = cms.string (''), # if blank, will attempt to use the SFs hard-coded in SFWeight.h
     muonSFFile = cms.string (os.environ['CMSSW_BASE']+'/src/OSUT3Analysis/Configuration/data/MuonSF_ID_ISO_2D.root'),
     dataPU = cms.string ('PU_data_190456_208686_69300xSec'),
-    electronSFID = cms.string ('mvaTrig0p9'),
+    electronSFID = cms.string ('mvaTrig0p9'), # ID label for using hard-coded SFs in SFWeight.h
+    electronSF = cms.string (''), # histogram name if using SFs from a histogram
     muonSF = cms.string ('Combined_TOT'),
     dataset = cms.string ('TTbar_Lep'),#dummy variable
     datasetType = cms.string ('bgMC'),#dummy variable
