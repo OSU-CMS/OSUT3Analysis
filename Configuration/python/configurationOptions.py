252909cd1311503af1bfcf4edf19c5456611de2d
Jamie Antonelli <lantonel@cern.ch>
2013-10-23 09:07:55 -0400

    updated the definition of the Background dataset

--- Configuration/python/configurationOptions.py
+++ Configuration/python/configurationOptions.py
@@ -441,32 +441,29 @@ composite_dataset_definitions = {
 
 
     'Background' : [
+        # w+jets
         'Wjets',
-        
+        # ttbar
         'TTbar_Had',
         'TTbar_SemiLep',
         'TTbar_Lep',
-        
+        # dy
         'DYToEE_20',
         'DYToMuMu_20',
         'DYToTauTau_20',
-        
+        # single top
+        'SingleT_s',
+        'SingleTbar_s',
+        'SingleT_t',
+        'SingleTbar_t',
+        'SingleT_tW',
+        'SingleTbar_tW',
+        # diboson
         'WW',
         'WZ',
         'ZZ',
-        
-#        'QCD_MuEnriched_15to20',
-        'QCD_MuEnriched_20to30',
-        'QCD_MuEnriched_30to50',
-        'QCD_MuEnriched_50to80',
-        'QCD_MuEnriched_80to120',
-        'QCD_MuEnriched_120to170',
-        'QCD_MuEnriched_170to300',
-        'QCD_MuEnriched_300to470',
-        'QCD_MuEnriched_470to600',
-        'QCD_MuEnriched_600to800',
-        'QCD_MuEnriched_800to1000',
-        'QCD_MuEnriched_1000',
+        'ZG',
+        'WG',
     ],
 
     #for disappTrks
