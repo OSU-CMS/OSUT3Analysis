3f555f8e05b95c018ab0e7ef33b358c839d31539
Wells Wulsin <wulsin.1@osu.edu>
2014-05-07 07:49:02 -0400

    Add comment about upper limits

--- AnaTools/bin/cutFlowLimits.cpp
+++ AnaTools/bin/cutFlowLimits.cpp
@@ -97,6 +97,8 @@ getLimits (TH1D *cutFlow, TDirectoryFile *dir)
       // "Poisson or binomal data", section 37.4.2.3 in 2013 version; Eqns. 37.71a, 37.71b.  
       // Here ALPHA is the confidence level, so ALPHA = 0.68 corresponds to a 68% C.L.
       // The PDG uses a different convention:  the C.L is 1 - \alpha.  
+      // For example, the 68% CL upper limit on an observation of 0 events is:
+      // 0.5 * TMath::ChisquareQuantile (0.68, 2) = 1.139  
       eventsLowerLimit = 0.5 * TMath::ChisquareQuantile (1 - ALPHA, 2 * events);
       eventsUpperLimit = 0.5 * TMath::ChisquareQuantile (ALPHA, 2 * (events + 1));
       lowerLimit->SetBinContent (i, eventsLowerLimit);
