5d203a47172b8fc9a816f0079814b4565e41d16b
Wells Wulsin <wulsin.1@osu.edu>
2014-10-17 10:00:53 -0400

    Add class definitions for userVariable map<string, double>

--- AnaTools/src/classes.h
+++ AnaTools/src/classes.h
@@ -3,6 +3,9 @@
 //Add includes for your classes here
 #include "OSUT3Analysis/AnaTools/interface/BNstop.h"
 #include "OSUT3Analysis/AnaTools/interface/BNPFChgHad.h"
+#include "OSUT3Analysis/AnaTools/interface/UserVars.h"
+
+
 #include <vector>
 
 namespace {
@@ -17,5 +20,16 @@ namespace {
      edm::Wrapper<BNPFChgHad> PFChgHaddummy1;
      std::vector<BNPFChgHad> PFChgHaddummy2;
      edm::Wrapper<std::vector<BNPFChgHad> > PFChgHaddummy3;
+
+     UserVars uservarsdummy0;
+     edm::Wrapper<UserVars> uservarsdummy1;
+     std::vector<UserVars> uservarsdummy2;
+     edm::Wrapper<std::vector<UserVars> > uservarsdummy3;
+
+     std::map<std::string, double> newvarsdummy0;
+     edm::Wrapper<std::map<std::string, double> > newvarsdummy1;
+     std::vector<std::map<std::string, double> > newvarsdummy2;
+     edm::Wrapper<std::vector<std::map<std::string, double> > > newvarsdummy3;
+
    };
 }
