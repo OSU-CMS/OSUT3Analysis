47a24ab3cd9e7987f3d226763c19e9d8409c10ba
Jamie Antonelli <lantonel@cern.ch>
2014-12-05 04:49:22 -0500

    fixed conflicts

--- AnaTools/plugins/Plotter.h
+++ AnaTools/plugins/Plotter.h
@@ -28,28 +28,9 @@ class Plotter : public edm::EDAnalyzer
       edm::ParameterSet collections_;
 
       //Collections
-<<<<<<< HEAD
-      edm::Handle<BNbxlumiCollection>         bxlumis;
-      edm::Handle<BNelectronCollection>       electrons;
-      edm::Handle<BNeventCollection>          events;
-      edm::Handle<BNgenjetCollection>         genjets;
-      edm::Handle<BNjetCollection>            jets;
-      edm::Handle<BNmcparticleCollection>     mcparticles;
-      edm::Handle<BNmetCollection>            mets;
-      edm::Handle<BNmuonCollection>           muons;
-      edm::Handle<BNmuonCollection>           secMuons;
-      edm::Handle<BNphotonCollection>         photons;
-      edm::Handle<BNprimaryvertexCollection>  primaryvertexs;
-      edm::Handle<BNsuperclusterCollection>   superclusters;
-      edm::Handle<BNtauCollection>            taus;
-      edm::Handle<BNtrackCollection>          tracks;
-      edm::Handle<BNtriggerCollection>        triggers;
-      edm::Handle<BNtrigobjCollection>        trigobjs;
-      edm::Handle<vector< map<string,double> > > userVariables;
 
-=======
       Collections handles_;
->>>>>>> 0ed760cc829edfa31add7458e6897bee183729ed
+
 
       vector<edm::ParameterSet> histogramSets_;
 
@@ -74,29 +55,12 @@ class Plotter : public edm::EDAnalyzer
       void bookHistogram(const HistoDef);
       pair<string,string> getVariableAndFunction(const string);
 
-<<<<<<< HEAD
-      template <class InputCollection> void fillHistogram(const histoDef, const InputCollection);
-      template <class InputCollection1, class InputCollection2> void fillHistogram(const histoDef,
-										   const InputCollection1,
-										   const InputCollection2);
-
-      template <class InputCollection> void fill1DHistogram(const histoDef, const InputCollection);
-      template <class InputCollection1, class InputCollection2> void fill1DHistogram(const histoDef,
-										     const InputCollection1,
-										     const InputCollection2);
-
-      template <class InputCollection> void fill2DHistogram(const histoDef, const InputCollection);
-      template <class InputCollection1, class InputCollection2> void fill2DHistogram(const histoDef,
-										     const InputCollection1,
-										     const InputCollection2);
-=======
       template <class InputCollection> void fillHistogram(const HistoDef, const InputCollection);
       template <class InputCollection1, class InputCollection2> void fillHistogram(const HistoDef, const InputCollection1, const InputCollection2);
 
       void fillHistogram(const HistoDef &);
       void fill1DHistogram(const HistoDef &);
       void fill2DHistogram(const HistoDef &);
->>>>>>> 0ed760cc829edfa31add7458e6897bee183729ed
 
       double getBinSize(TH1D *, const double);
       pair<double,double> getBinSize(TH2D *, const double, const double);
@@ -107,3 +71,7 @@ class Plotter : public edm::EDAnalyzer
 };
 
 #endif
+
+
+
+
