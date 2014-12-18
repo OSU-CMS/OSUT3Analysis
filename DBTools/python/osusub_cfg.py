68025ddb3d019a3ef1bd79c769a5b105401907be
bing <bingxuan.liu@cern.ch>
2014-12-18 12:00:33 -0500

    Assigne files to each condor job more efficiently.

--- DBTools/python/osusub_cfg.py
+++ DBTools/python/osusub_cfg.py
@@ -7,8 +7,12 @@ if len (sys.argv) == 7 and sys.argv[2] == "True":
   jobNumber = int (sys.argv[4])
   if int (sys.argv[3]) != 0 and sys.argv[5] != "NULL":
     exec("import datasetInfo_" + Label +"_cfg as datasetInfo")
-    filesPerJob = int (math.ceil (len (datasetInfo.listOfFiles) / nJobs))
-    runList = datasetInfo.listOfFiles[(jobNumber * filesPerJob):(jobNumber * filesPerJob + filesPerJob)]
+    filesPerJob = int (math.floor (len (datasetInfo.listOfFiles) / nJobs))
+    residualLength = int(len(datasetInfo.listOfFiles)%nJobs)
+    if jobNumber < residualLength:  
+        runList = datasetInfo.listOfFiles[(jobNumber * filesPerJob + jobNumber):(jobNumber * filesPerJob + filesPerJob + jobNumber + 1)]
+    else:
+	runList = datasetInfo.listOfFiles[(jobNumber * filesPerJob + residualLength):(jobNumber * filesPerJob + residualLength + filesPerJob)]
   dataset = sys.argv[5]
   datasetLabel = sys.argv[6]
   batchMode = True
