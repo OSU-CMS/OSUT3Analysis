83484cc77093918eeb7ccfa5a18d1c78828c8a43
Wells Wulsin <wulsin.1@osu.edu>
2015-10-07 12:43:37 -0400

    Make colors bold

--- Configuration/scripts/makeComparisonPlots.py
+++ Configuration/scripts/makeComparisonPlots.py
@@ -146,11 +146,11 @@ header_y_top     = 0.9947552
 
 colors = {
     'black'  : 1,
-    'red'    : 623,
-    'green'  : 407,
-    'purple' : 871,
-    'blue'   : 591,
-    'yellow' : 393,
+    'red'    : 632,
+    'green'  : 416,
+    'purple' : 880,
+    'blue'   : 600,
+    'yellow' : 400,
 }
 
 colorList = [
