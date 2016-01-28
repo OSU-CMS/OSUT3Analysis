11480e9133012edfc3d35ef6ed4b061e773b5f72
allenji <weifengji@yahoo.com>
2016-01-28 16:30:46 -0500

    wipe out the annoying output

--- Collections/plugins/OSUDtsegProducer.cc
+++ Collections/plugins/OSUDtsegProducer.cc
@@ -7,13 +7,10 @@
 OSUDtsegProducer::OSUDtsegProducer (const edm::ParameterSet &cfg) :
   collections_ (cfg.getParameter<edm::ParameterSet> ("collections"))
 {
-  std::cout << "Begining " << std::endl;
   collection_ = collections_.getParameter<edm::InputTag> ("dtsegs");
 
-  std::cout << "Middle" << std::endl; 
   produces<vector<osu::Dtseg> > (collection_.instance ());
   std::cout << collection_.instance() << std::endl;
-  std::cout << "end of constructor" << std::endl;
 }
 
 OSUDtsegProducer::~OSUDtsegProducer ()
@@ -23,13 +20,11 @@ OSUDtsegProducer::~OSUDtsegProducer ()
 void
 OSUDtsegProducer::produce (edm::Event &event, const edm::EventSetup &setup)
 {
-  std::cout << "before we do any thing" << std::endl;
   edm::Handle<vector<TYPE (dtsegs)> > collection;
   if (!anatools::getCollection (collection_, collection, event, false))
     return;
 
   pl_ = auto_ptr<vector<osu::Dtseg> > (new vector<osu::Dtseg> ());
-  std::cout << "before push back" << std::endl;
   for (const auto &object : *collection)
     {
       const osu::Dtseg dtseg (object);
