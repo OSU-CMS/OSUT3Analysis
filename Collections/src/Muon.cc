4768616cca8e9e8e900ee5464a551d1263890f6d
allenji <weifengji@yahoo.com>
2016-01-28 20:58:45 -0500

    modified osu::muon collection to be compatible with AOD_CUSTOM

--- Collections/src/Muon.cc
+++ Collections/src/Muon.cc
@@ -1,7 +1,7 @@
 #include "OSUT3Analysis/Collections/interface/Muon.h"
 
 #if IS_VALID(muons)
-
+#if DATA_FORMAT == MINI_AOD || DATA_FORMAT == MINI_AOD_CUSTOM || DATA_FORMAT == AOD
 osu::Muon::Muon ()
 {
   isTightMuonWRTVtx_ = false;
@@ -25,13 +25,24 @@ osu::Muon::Muon (const TYPE(muons) &muon, const edm::Handle<vector<osu::Mcpartic
   isTightMuonWRTVtx_ = false;
 }
 
-osu::Muon::~Muon ()
-{
-}
 
 void osu::Muon::set_isTightMuonWRTVtx (const bool isTightMuon)
 {
   isTightMuonWRTVtx_ = isTightMuon;
+}
+#elif DATA_FORMAT == AOD_CUSTOM
+osu::Muon::Muon ()
+{
+}
+
+osu::Muon::Muon (const TYPE(muons) &muon) :
+    TYPE(muons) (muon)
+{
+}
+#endif
+
+osu::Muon::~Muon ()
+{
 }
 
 
