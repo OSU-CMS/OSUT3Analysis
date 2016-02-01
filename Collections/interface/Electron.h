37f58f6bc31159a23e2edfe2a5a1cd2a240906d9
Andrew Hart <ahart@cern.ch>
2016-02-01 10:08:55 -0500

    Added function for ID.

--- Collections/interface/Electron.h
+++ Collections/interface/Electron.h
@@ -21,10 +21,12 @@ namespace osu
         const float AEff () const;
         const float rho() const;
         const bool vtxFitConversion() const;
+        const bool passesTightID_noIsolation () const;
         void set_rho (float value) { rho_  = value; }  
         void set_AEff (float value) { AEff_  = value; }  
         void set_missingInnerHits(int value) { missingInnerHits_ = value; }  
         void set_vtxFitConversion(bool value) { vtxFitConversion_ = value; }  
+        void set_passesTightID_noIsolation (const reco::BeamSpot &, const TYPE(primaryvertexs) &, const edm::Handle<vector<reco::Conversion> > &);
 #else
   class Electron : public GenMatchable<TYPE(electrons), 11>
     {
@@ -42,6 +44,7 @@ namespace osu
         float rho_;
         int missingInnerHits_;
         float AEff_;
+        bool passesTightID_noIsolation_;
 #endif
        
     };
