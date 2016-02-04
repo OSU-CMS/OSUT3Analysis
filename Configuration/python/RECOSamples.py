71056a0fdd7c571689eaba62f3a63f1cb6beec6a
allenji <weifengji@yahoo.com>
2016-02-03 23:02:50 -0500

    move dataset definition to RECOSample.py, add cfgoption of stopped particles analysis samples to configurationOptions.py

---
+++ Configuration/python/RECOSamples.py
@@ -0,0 +1,17 @@
+dataset_names = {
+################### SP ntuples stored on T3 ###############
+####################signal samples#########################
+    'GluinoToUUbarChi0_1200_200'  : "/HSCPgluino_UUbarChi0_13TeV-pythia6/RunIISpring15-74x_mcRun2_StoppPtls_gluino_1200_chi0_200/USER",
+    'GluinoToUUbarChi0_1200_600'  : "/HSCPgluino_UUbarChi0_13TeV-pythia6/RunIISpring15-74x_mcRun2_StoppPtls_gluino_1200_chi0_600/USER",
+    'GluinoToUUbarChi0_1200_1000'  : "/HSCPgluino_UUbarChi0_13TeV-pythia6/RunIISpring15-74x_mcRun2_StoppPtls_gluino_1200_chi0_1000/USER",
+    'GluinoToGChi0_1200_200'  : "/HSCPgluino_gChi0_13TeV-pythia6/RunIISpring15-74x_mcRun2_StoppPtls_gluino_1200_chi0_200/USER",
+    'GluinoToGChi0_1200_400'  : "/HSCPgluino_gChi0_13TeV-pythia6/RunIISpring15-74x_mcRun2_StoppPtls_gluino_1200_chi0_400/USER",
+    'GluinoToGChi0_1200_600'  : "/HSCPgluino_gChi0_13TeV-pythia6/RunIISpring15-74x_mcRun2_StoppPtls_gluino_1200_chi0_600/USER",
+    'GluinoToGChi0_1200_800'  : "/HSCPgluino_gChi0_13TeV-pythia6/RunIISpring15-74x_mcRun2_StoppPtls_gluino_1200_chi0_800/USER",
+    'GluinoToGChi0_1200_1000'  : "/HSCPgluino_gChi0_13TeV-pythia6/RunIISpring15-74x_mcRun2_StoppPtls_gluino_1200_chi0_1000/USER",
+
+################## SP ntuples no stored on T3 ############
+########################## data ##########################
+    'NoBPTX_2015D_v3' : "/NoBPTX/wji-Run2015D-PromptReco-v3_SP-v2p0-f01519ca7e946644c73f465aa3632aa9/USER",
+    'NoBPTX_2015D_v4' : "/NoBPTX/wji-Run2015D-PromptReco-v4_SP-v2p0-f01519ca7e946644c73f465aa3632aa9/USER",
+}
