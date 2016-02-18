4e20b37c2935320340752b5815a97c386dc14330
Andrew Hart <ahart@cern.ch>
2016-02-18 11:48:07 -0500

    Added an extern statement so that ObjectSelector is not instantiated here. This is so the linker finds the specially defined version in libOSUT3AnalysisAnaTools.so.

--- AnaTools/plugins/BeamspotObjectSelector.cc
+++ AnaTools/plugins/BeamspotObjectSelector.cc
@@ -1,6 +1,8 @@
 #include "OSUT3Analysis/AnaTools/interface/ObjectSelector.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
+extern template class ObjectSelector<osu::Beamspot, TYPE(beamspots)>;
+
 #if IS_VALID(beamspots)
 typedef ObjectSelector<osu::Beamspot, TYPE(beamspots)> BeamspotObjectSelector;
   DEFINE_FWK_MODULE(BeamspotObjectSelector);
