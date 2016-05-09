50df75603e137ec9a33223d8c7a3a95e62c6fd3a
Juliette Alimena <juliette.alimena@cern.ch>
2016-05-09 07:36:39 -0400

    add mchamp samples for delayed muons

--- Configuration/python/GENSIMSamples.py
+++ Configuration/python/GENSIMSamples.py
@@ -1,5 +1,5 @@
 dataset_names = {
-################### Stopped Particles stage 1 GEN-SIM not stored on T3 ###############
+################### Stopped Particles stage 1 GEN-SIM stored on T3 ###############
     'mchamp100'   : "/HSCPmchamp6_M-100_TuneZ2star_13TeV-pythia6/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
     'mchamp200'   : "/HSCPmchamp6_M-200_TuneZ2star_13TeV-pythia6/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
     'mchamp400'   : "/HSCPmchamp6_M-400_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
@@ -10,4 +10,17 @@ dataset_names = {
     'mchamp1800'  : "/HSCPmchamp6_M-1800_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
     'mchamp2200'  : "/HSCPmchamp6_M-2200_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
     'mchamp2600'  : "/HSCPmchamp6_M-2600_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+
+################### Stopped Particles stage 2 GEN-SIM stored on T3 ###############
+    #'mchamp100_Stage2SeparateEventsParticle0'   : "/HSCPmchamp6_M-100_TuneZ2star_13TeV-pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
+    #'mchamp200_Stage2SeparateEventsParticle0'   : "/HSCPmchamp6_M-200_TuneZ2star_13TeV-pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
+    #'mchamp400_Stage2SeparateEventsParticle0'   : "/HSCPmchamp6_M-400_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
+    'mchamp600_Stage2SeparateEventsParticle0'   : "/HSCPmchamp6_M-600_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
+    #'mchamp800_Stage2SeparateEventsParticle0'   : "/HSCPmchamp6_M-800_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
+    #'mchamp1000_Stage2SeparateEventsParticle0'  : "/HSCPmchamp6_M-1000_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
+    #'mchamp1400_Stage2SeparateEventsParticle0'  : "/HSCPmchamp6_M-1400_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
+    #'mchamp1800_Stage2SeparateEventsParticle0'  : "/HSCPmchamp6_M-1800_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
+    #'mchamp2200_Stage2SeparateEventsParticle0'  : "/HSCPmchamp6_M-2200_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
+    #'mchamp2600_Stage2SeparateEventsParticle0'  : "/HSCPmchamp6_M-2600_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
+
 }
---
+++ Configuration/python/RAWSIMSamples.py
@@ -0,0 +1,14 @@
+dataset_names = {
+################### Stopped Particles stage 2 DIGI-HLT samples stored on T3 ###############
+    #'mchamp100_DigiHltSeparateEventsParticle0'   : "/HSCPmchamp6_M-100_TuneZ2star_13TeV-pythia6/RunIIWinter15GS-MCRUN2_765_Stage2DigiHltSeparateEventsParticle0/USER",
+    #'mchamp200_DigiHltSeparateEventsParticle0'   : "/HSCPmchamp6_M-200_TuneZ2star_13TeV-pythia6/RunIIWinter15GS-MCRUN2_765_Stage2DigiHltSeparateEventsParticle0/USER",
+    #'mchamp400_DigiHltSeparateEventsParticle0'   : "/HSCPmchamp6_M-400_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_765_Stage2DigiHltSeparateEventsParticle0/USER",
+    'mchamp600_DigiHltSeparateEventsParticle0'   : "/HSCPmchamp6_M-600_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_765_Stage2DigiHltSeparateEventsParticle0/USER",
+    #'mchamp800_DigiHltSeparateEventsParticle0'   : "/HSCPmchamp6_M-800_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_765_Stage2DigiHltSeparateEventsParticle0/USER",
+    #'mchamp1000_DigiHltSeparateEventsParticle0'  : "/HSCPmchamp6_M-1000_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_765_Stage2DigiHltSeparateEventsParticle0/USER",
+    #'mchamp1400_DigiHltSeparateEventsParticle0'  : "/HSCPmchamp6_M-1400_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_765_Stage2DigiHltSeparateEventsParticle0/USER",
+    #'mchamp1800_DigiHltSeparateEventsParticle0'  : "/HSCPmchamp6_M-1800_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_765_Stage2DigiHltSeparateEventsParticle0/USER",
+    #'mchamp2200_DigiHltSeparateEventsParticle0'  : "/HSCPmchamp6_M-2200_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_765_Stage2DigiHltSeparateEventsParticle0/USER",
+    #'mchamp2600_DigiHltSeparateEventsParticle0'  : "/HSCPmchamp6_M-2600_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-MCRUN2_765_Stage2DigiHltSeparateEventsParticle0/USER",
+
+}
