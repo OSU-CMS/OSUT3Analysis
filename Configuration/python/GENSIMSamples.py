96ba415976a36209f8f3eae1dde42e4f76910142
Juliette Alimena <juliette.alimena@cern.ch>
2016-06-20 10:01:19 -0400

    add gluino-->qqbarchi0_2, chi0_2-->llchi0 model for Delayed Muons

--- Configuration/python/GENSIMSamples.py
+++ Configuration/python/GENSIMSamples.py
@@ -11,6 +11,21 @@ dataset_names = {
     'mchamp2200'  : "/HSCPmchamp6_M-2200_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
     'mchamp2600'  : "/HSCPmchamp6_M-2600_TuneZ2star_13TeV_pythia6/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
 
+    'gluino100'   : "/HSCPgluino_M-100_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino200'   : "/HSCPgluino_M-200_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino400'   : "/HSCPgluino_M-400_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino600'   : "/HSCPgluino_M-600_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino800'   : "/HSCPgluino_M-800_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino1000'  : "/HSCPgluino_M-1000_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino1200'  : "/HSCPgluino_M-1200_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino1400'  : "/HSCPgluino_M-1400_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino1600'  : "/HSCPgluino_M-1600_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino1800'  : "/HSCPgluino_M-1800_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino2000'  : "/HSCPgluino_M-2000_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino2200'  : "/HSCPgluino_M-2200_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino2400'  : "/HSCPgluino_M-2400_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+    'gluino2600'  : "/HSCPgluino_M-2600_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-HSCP_customise_MCRUN2_71_V1-v2/GEN-SIM",
+
 ################### Stopped Particles stage 2 GEN-SIM stored on T3 ###############
     #'mchamp100_Stage2SeparateEventsParticle0'   : "/HSCPmchamp6_M-100_TuneZ2star_13TeV-pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
     #'mchamp200_Stage2SeparateEventsParticle0'   : "/HSCPmchamp6_M-200_TuneZ2star_13TeV-pythia6/RunIIWinter15GS-MCRUN2_7122_Stage2GenSimSeparateEventsParticle0/USER",
