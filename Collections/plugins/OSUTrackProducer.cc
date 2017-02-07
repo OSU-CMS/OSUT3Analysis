850d8af4db7ca849fac06b0774745f78dbebce81
Juliette Alimena <juliette.alimena@cern.ch>
2017-02-07 03:52:12 -0500

    comment out unused variable so package compiles

--- Collections/plugins/OSUTrackProducer.cc
+++ Collections/plugins/OSUTrackProducer.cc
@@ -305,7 +305,7 @@ OSUTrackProducer::extractFiducialMap (const edm::ParameterSet &cfg, EtaPhiList &
       for (int j = 1; j <= afterVetoHist->GetYaxis ()->GetNbins (); j++)
         {
           double content = afterVetoHist->GetBinContent (i, j),
-                 error = afterVetoHist->GetBinError (i, j),
+	    //error = afterVetoHist->GetBinError (i, j),
                  eta = afterVetoHist->GetXaxis ()->GetBinCenter (i),
                  phi = afterVetoHist->GetYaxis ()->GetBinCenter (j);
 
