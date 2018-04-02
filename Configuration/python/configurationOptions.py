59506f6f07d72966e65f1a5efcf36b653c6a7e5c
Juliette Alimena <juliette.alimena@cern.ch>
2018-04-02 07:54:24 -0400

    correct label

--- Configuration/python/configurationOptions.py
+++ Configuration/python/configurationOptions.py
@@ -6237,7 +6237,7 @@ labels = {
     ############################################################################
     #DY
     'DYJetsToLL_50'     : "Z#rightarrowl^{+}l^{-} M(50)",
-    'DYJetsToLL'        : "Z#rightarrowl^{+}l^{-} M(10)",
+    'DYJetsToLL'        : "Z#rightarrowl^{+}l^{-}",
 
     #ZJetsToNuNu
     'DYJetsToNuNu'   :  "Z#rightarrow#nu#bar{#nu}",
