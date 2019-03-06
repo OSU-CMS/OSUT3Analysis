48c670b556e246c26f1be1a908d5e99ec50f37b6
Brian Francis <brian.patrick.francis@cern.ch>
2019-03-06 16:13:03 -0500

    Change var names everywhere

--- Collections/src/Electron.cc
+++ Collections/src/Electron.cc
@@ -295,26 +295,26 @@ osu::Electron::set_passesTightID_noIsolation_LegacySpring15 (const reco::BeamSpo
 
   // not 94X -- actually corresponds to https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedElectronIdentificationRun2Archive#Spring15_selection_25ns
   if (fabs (this->superCluster ()->eta ()) <= 1.479) {
-      passesTightID_noIsolation_ = (this->full5x5_sigmaIetaIeta ()                                                        <   0.0101  &&
-                                    fabs (this->deltaEtaSuperClusterTrackAtVtx ())                                        <   0.00926 &&
-                                    fabs (this->deltaPhiSuperClusterTrackAtVtx ())                                        <   0.0336  &&
-                                    this->hadronicOverEm ()                                                               <   0.0597  &&
-                                    fabs (1.0 / this->ecalEnergy () - this->eSuperClusterOverP () / this->ecalEnergy ())  <   0.012   &&
-                                    fabs (this->gsfTrack ()->dxy (vertex.position ()))                                    <   0.0111  &&
-                                    fabs (this->gsfTrack ()->dz (vertex.position ()))                                     <   0.0466  &&
-                                    this->gsfTrack ()->hitPattern ().numberOfHits (reco::HitPattern::MISSING_INNER_HITS)  <=  2       &&
-                                    !ConversionTools::hasMatchedConversion (*this, conversions, beamspot.position ()));
+      passesTightID_noIsolation_LegacySpring15_ = (this->full5x5_sigmaIetaIeta ()                                                        <   0.0101  &&
+                                                   fabs (this->deltaEtaSuperClusterTrackAtVtx ())                                        <   0.00926 &&
+                                                   fabs (this->deltaPhiSuperClusterTrackAtVtx ())                                        <   0.0336  &&
+                                                   this->hadronicOverEm ()                                                               <   0.0597  &&
+                                                   fabs (1.0 / this->ecalEnergy () - this->eSuperClusterOverP () / this->ecalEnergy ())  <   0.012   &&
+                                                   fabs (this->gsfTrack ()->dxy (vertex.position ()))                                    <   0.0111  &&
+                                                   fabs (this->gsfTrack ()->dz (vertex.position ()))                                     <   0.0466  &&
+                                                   this->gsfTrack ()->hitPattern ().numberOfHits (reco::HitPattern::MISSING_INNER_HITS)  <=  2       &&
+                                                   !ConversionTools::hasMatchedConversion (*this, conversions, beamspot.position ()));
   }
   else if (fabs (this->superCluster ()->eta ()) < 2.5) {
-      passesTightID_noIsolation_ = (this->full5x5_sigmaIetaIeta ()                                                        <   0.0279  &&
-                                    fabs (this->deltaEtaSuperClusterTrackAtVtx ())                                        <   0.00724 &&
-                                    fabs (this->deltaPhiSuperClusterTrackAtVtx ())                                        <   0.0918  &&
-                                    this->hadronicOverEm ()                                                               <   0.0615  &&
-                                    fabs (1.0 / this->ecalEnergy () - this->eSuperClusterOverP () / this->ecalEnergy ())  <   0.00999 &&
-                                    fabs (this->gsfTrack ()->dxy (vertex.position ()))                                    <   0.0351  &&
-                                    fabs (this->gsfTrack ()->dz (vertex.position ()))                                     <   0.417   &&
-                                    this->gsfTrack ()->hitPattern ().numberOfHits (reco::HitPattern::MISSING_INNER_HITS)  <=  1       &&
-                                    !ConversionTools::hasMatchedConversion (*this, conversions, beamspot.position ()));
+      passesTightID_noIsolation_LegacySpring15_ = (this->full5x5_sigmaIetaIeta ()                                                        <   0.0279  &&
+                                                   fabs (this->deltaEtaSuperClusterTrackAtVtx ())                                        <   0.00724 &&
+                                                   fabs (this->deltaPhiSuperClusterTrackAtVtx ())                                        <   0.0918  &&
+                                                   this->hadronicOverEm ()                                                               <   0.0615  &&
+                                                   fabs (1.0 / this->ecalEnergy () - this->eSuperClusterOverP () / this->ecalEnergy ())  <   0.00999 &&
+                                                   fabs (this->gsfTrack ()->dxy (vertex.position ()))                                    <   0.0351  &&
+                                                   fabs (this->gsfTrack ()->dz (vertex.position ()))                                     <   0.417   &&
+                                                   this->gsfTrack ()->hitPattern ().numberOfHits (reco::HitPattern::MISSING_INNER_HITS)  <=  1       &&
+                                                   !ConversionTools::hasMatchedConversion (*this, conversions, beamspot.position ()));
   }
 }
 
