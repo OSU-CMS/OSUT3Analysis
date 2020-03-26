53c40504c092fb8237ae81f9d136b0bd800dac6c
Bryan Cardwell <bryan.cardwell@cern.ch>
2020-03-26 13:41:44 -0500

    Add uniqueMotherPdgId to McParticle

--- Collections/src/Mcparticle.cc
+++ Collections/src/Mcparticle.cc
@@ -7,8 +7,15 @@ osu::Mcparticle::Mcparticle ()
 }
 
 osu::Mcparticle::Mcparticle (const TYPE(mcparticles) &mcparticle) :
-  TYPE(mcparticles) (mcparticle)
+  TYPE(mcparticles)  (mcparticle),
+  uniqueMotherPdgId_ (INVALID_VALUE)
 {
+}
+
+const int
+osu::Mcparticle::uniqueMotherPdgId () const
+{
+  return uniqueMotherPdgId_;
 }
 
 osu::Mcparticle::~Mcparticle ()
