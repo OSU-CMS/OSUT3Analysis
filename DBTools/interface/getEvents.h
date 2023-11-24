f9ac11740d67caf3751b3b4e18e7aa7df0976c13
Michael Carrigan <carriganm95@gmail.com>
2023-11-24 13:19:42 -0500

    remove unused headers and functions

--- DBTools/interface/getEvents.h
+++ DBTools/interface/getEvents.h
@@ -1,10 +1,8 @@
 #include "TFile.h"
 #include "TTree.h"
-//#include "EventAuxiliary.h"
 #include "DataFormats/FWLite/interface/Handle.h"
 #include "DataFormats/FWLite/interface/Event.h"
 #include "FWCore/FWLite/interface/FWLiteEnabler.h"
-//#include "OSUT3Analysis/DBTools/interface/json.h"
 #include <iostream>
 #include <fstream>
 #include <TSystem.h>
@@ -25,8 +23,4 @@ struct runInfo {
 
 
 std::vector<runInfo> getEventsInFile(std::string filename);
-
-//void getMiniAODFile();
-
-//int getEvents();
 
