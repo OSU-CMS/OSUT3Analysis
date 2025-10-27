997c969491e907a6b4b2fd8b9efa2d812254197d
Ryan De Los Santos <ryansantos1174@gmail.com>
2025-10-27 14:41:51 -0500

    Update git checkout script to python3

--- githooks/post-checkout.d/deletePycFiles.py
+++ githooks/post-checkout.d/deletePycFiles.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
-import sys, os, re
-from subprocess import check_output
+#!/usr/bin/env python3
+import sys
+import os
 
 # Collect the parameters
 previous_head = sys.argv[1]
 new_head = sys.argv[2]
 is_branch_checkout = sys.argv[3]
 
-#A common problem with Python developers occurs when generated .pyc files
-#stick around after switching branches. The interpreter sometimes uses these
-#.pyc instead of the .py source file. To avoid any confusion, you can delete
-#all .pyc files every time you check out a new branch using this post-checkout script:
+# A common problem with Python developers occurs when generated .pyc files
+# stick around after switching branches. The interpreter sometimes uses these
+# .pyc instead of the .py source file. To avoid any confusion, delete
+# all .pyc files every time you check out a new branch using this post-checkout script.
 if is_branch_checkout == "0":
-    print "post-checkout: This is a file checkout. Nothing to do."
+    print("post-checkout: This is a file checkout. Nothing to do.")
     sys.exit(0)
 
-print "post-checkout: Deleting all '.pyc' files in working directory"
+print("post-checkout: Deleting all '.pyc' files in working directory")
 for root, dirs, files in os.walk('.'):
     for filename in files:
-        ext = os.path.splitext(filename)[1]
-        if ext == '.pyc':
+        if filename.endswith('.pyc'):
             os.unlink(os.path.join(root, filename))
