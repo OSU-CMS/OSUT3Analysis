4c862af2e32ca2e593803d7ccf0e4a35061ed27c
Lucas Nestor <nestor.40@osu.edu>
2025-11-17 10:45:05 -0500

    Update JERC corrections to centrally-maintained recommendation (#286)
    * Switch jet energy corrections to centrally-maintained version
    
    * Update JERC corrections to use POG-supplied files
    
    The POG recommended to copy the JecConfigReader, JecApplication,
    and JeCConfigAK4 files and use that as your JERC correction
    implementation.
    
    https://cms-jerc.web.cern.ch/ApplicationTutorial
    https://gitlab.cern.ch/cms-analysis/jme/jerc-application-tutorial
    
    * Use proper total JES uncertainty tag
    
    * Remove unused OSUJetProduer configuration
    
    * Add hardcoded JEC config file path since it is static
    
    * Update eraTag naming to be consistent with DisappTrks

--- Configuration/python/CollectionProducer_cff.py
+++ Configuration/python/CollectionProducer_cff.py
@@ -138,27 +138,16 @@ copyConfiguration (collectionProducer.genjets, collectionProducer.genMatchables)
 
 collectionProducer.jets = cms.EDProducer ("OSUJetProducer",
     rho = cms.InputTag("fixedGridRhoFastjetAll", "", ""),
-    jetResolutionPayload = cms.FileInPath("OSUT3Analysis/Collections/data/Fall15_25nsV2_MC_PtResolution_AK4PFchs.txt"),
-    jetResSFPayload = cms.FileInPath("OSUT3Analysis/Collections/data/Fall15_25nsV2_MC_SF_AK4PFchs.txt"),
-    jetResFromGlobalTag = cms.bool(False),
-    jetResNewPrescription = cms.bool(False),
-    jecjerFile = cms.FileInPath("OSUT3Analysis/Collections/data/JetEnergyCorrections/Summer22EE_AK4PFPuppi.root"),
-    dataPeriod = cms.string("Summer22EE"),
-    dataEra = cms.string("_RunF"),
+    eraTag = cms.string(""),
+    year = cms.string("CHANGEME"),
+    isData = cms.bool(False),
+    jecConfigFile = cms.FileInPath("OSUT3Analysis/Collections/data/JecConfigAK4.json")
 )
 
 if os.environ["CMSSW_VERSION"].startswith ("CMSSW_8_0_") or os.environ["CMSSW_VERSION"].startswith ("CMSSW_9_4_") or os.environ["CMSSW_VERSION"].startswith ("CMSSW_10_2_"):
-    collectionProducer.jets.jetResFromGlobalTag = cms.bool(True)
     collectionProducer.jets.jetResNewPrescription = cms.bool(True)
 
 if os.environ["CMSSW_VERSION"].startswith ("CMSSW_12_4_") or os.environ["CMSSW_VERSION"].startswith ("CMSSW_13_0_"):
-
-    # Used for AK4CHS jets
-    # collectionProducer.jets.jetCorrectionPayload = cms.string("AK4PFchs")
-
-    # Used for AK4PUPPI jets
-    collectionProducer.jets.jetCorrectionPayload = cms.string("AK4PFPuppi")
-
     collectionProducer.jets.jetResNewPrescription = cms.bool(True)
 
 copyConfiguration (collectionProducer.jets, collectionProducer.genMatchables)
